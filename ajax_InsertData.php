cefee0520d99c752f53e9b50118fbf5cd396563f
developermkarim <m.karimcu@gmail.com>
2022-06-07 16:07:17 +0600

    php ajax crud starting

---
+++ ajax_InsertData.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST['stname'];
+$email = $_POST['stemail'];
+$location = $_POST['stlocation'];
+$date = $_POST['stdate'];
+$phone = $_POST['stphone'];
+
+$dbcon_insert = new mysqli('localhost','root','','ajax_in_crud') or die('couldnt connect to database');
+$stmt = "INSERT INTO students(Name,Email,Location,Date,Phone) VALUES('{$name}','{$email}','{$location}','{$date}',{$phone})";
+if ($dbcon_insert->query($stmt)){
+    echo 1;
+
+}else{
+    echo 0;
+}
+?>
