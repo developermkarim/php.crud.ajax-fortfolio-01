1e4b08d8ef83306ab1ae9074b5d9f8d751fb5c95
developermkarim <m.karimcu@gmail.com>
2022-06-07 20:51:48 +0600

    modal box show and hide

--- index.php
+++ index.php
@@ -115,80 +115,21 @@ $(document).on('click','.delete-btn',function(){
     }
   })
 })
-    //Delete Records
-    // $(document).on("click",".delete-btn", function(){
-    //   if(confirm("Do you really want to delete this record ?")){
-    //     var studentId = $(this).data("id");
-    //     var element = this;
+    
+    //  show Modal in Update data  Box Here
+$(document).on('click','.edit-btn',function(){
+  $('#modal').show();
+  
+  $(document).on('click','#close-btn',function(){
+    $('#modal').hide();
+  })
+})
+    //  Close Modal in Update data  Box Here
+   
+    // Update data in showing Modal Box Here
+    // Update data in showing Modal Box Here
 
-    //     $.ajax({
-    //       url: "ajax-delete.php",
-    //       type : "POST",
-    //       data : {id : studentId},
-    //       success : function(data){
-    //           if(data == 1){
-    //             $(element).closest("tr").fadeOut();
-    //           }else{
-    //             $("#error-message").html("Can't Delete Record.").slideDown();
-    //             $("#success-message").slideUp();
-    //           }
-    //       }
-    //     });
-    //   }
-    // });
 
-    //Show Modal Box
-    // $(document).on("click",".edit-btn", function(){
-    //   $("#modal").show();
-    //   var studentId = $(this).data("eid");
-
-    //   $.ajax({
-    //     url: "load-update-form.php",
-    //     type: "POST",
-    //     data: {id: studentId },
-    //     success: function(data) {
-    //       $("#modal-form table").html(data);
-    //     }
-    //   })
-    // });
-
-    //Hide Modal Box
-    // $("#close-btn").on("click",function(){
-    //   $("#modal").hide();
-    // });
-
-    //Save Update Form
-      // $(document).on("click","#edit-submit", function(){
-      //   var stuId = $("#edit-id").val();
-      //   var fname = $("#edit-fname").val();
-      //   var lname = $("#edit-lname").val();
-
-      //   $.ajax({
-      //     url: "ajax-update-form.php",
-      //     type : "POST",
-      //     data : {id: stuId, first_name: fname, last_name: lname},
-      //     success: function(data) {
-      //       if(data == 1){
-      //         $("#modal").hide();
-      //         loadTable();
-      //       }
-      //     }
-      //   })
-      // });
-
-    // Live Search
-    //  $("#search").on("keyup",function(){
-    //    var search_term = $(this).val();
-
-    //    $.ajax({
-    //      url: "ajax-live-search.php",
-    //      type: "POST",
-    //      data : {search:search_term },
-    //      success: function(data) {
-    //        $("#table-data").html(data);
-    //      }
-    //    });
-    //  });
   });
 </script>
 </body>
